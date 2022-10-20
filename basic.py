eb95a6165d91981af59c2adc77586ce0575c8fa3
lanemunds <lanemunds@gmail.com>
2022-10-20 17:02:47 -0600

    jinja

--- basic.py
+++ basic.py
@@ -1,13 +1,14 @@
+from turtle import pu
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = "lane"
-    letters = list(name)
-    pup_dict = {'pup_name':'Sammy'}
-    return render_template('basic.html', name=name, letters = letters, pup_dict = pup_dict)
+    mylist= [1,2,3,4,5]
+    puppies = ["fluffy","Ghost","Hobbs"]
+    user_logged_in = False 
+    return render_template('basic.html', mylist = mylist, puppies = puppies, user_logged_in = user_logged_in)
 
 
 if __name__=='__main__':
