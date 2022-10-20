cbe2bf39563e4067bc8863d931b0e605e22d3e7c
lanemunds <lanemunds@gmail.com>
2022-10-20 16:47:59 -0600

    initial commit

---
+++ basic.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "lane"
+    letters = list(name)
+    pup_dict = {'pup_name':'Sammy'}
+    return render_template('basic.html', name=name, letters = letters, pup_dict = pup_dict)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
